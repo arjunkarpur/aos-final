391a5fd8ac6e0edfae279b016f612ca85c541f63
Arjun Karpur <arjun.karpur@gmail.com>
2019-04-17 12:24:08 -0500

    Fixed bug related to aio_context creation

--- fs/create.py
+++ fs/create.py
@@ -1,6 +1,6 @@
 
 import os
 
-file_size = 1024;
+file_size = 1024*1024;
 with open("one.txt", "wb") as f:
     f.write(os.urandom(file_size));
--- src/single_copy_aio.c
+++ src/single_copy_aio.c
@@ -7,40 +7,40 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <sys/mman.h>
 
 int do_copy(char *src_fp, char *dest_fp)
 {
     // Perform copy of single file using AIO
 
     // Create io_context
-    int ret;
-    io_context_t aio_context;
-    ret = io_setup(1, &aio_context);
+    io_context_t aio_context = 0;
+    if (io_setup(100, &aio_context))
+    {
+        perror("Failed to create aio context...");
+        return -1;
+    }
 
     // Get src file size and create buffer
     int src_fd = open(src_fp, O_DIRECT, "rb");
     if (src_fd < 0)
     {
-        printf("Failed to open src file...EXITING\n");
+        perror("Failed to open src file...");
         return -1;
     }
-    int fsize = 1024;
-    char *buffer = malloc(fsize);
-
-    /*
-    //TODO: why does this cause submission of read req to fail?
-    //TODO:   see notes about aligning to pages when using O_DIRECT
-    int test[23];
-    int test2[23];
-
     struct stat f_stat;
     if (fstat(src_fd, &f_stat) != 0)
     {
-        printf("Failed to get src file size...EXITING\n");
+        perror("Failed to get src file size...");
         return -1;
     }
     int fsize = f_stat.st_size;
-    */
+    char *buffer = mmap(NULL, fsize, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+    if (!buffer)
+    {
+        perror("Failed to mmap buffer...");
+        return -1;
+    }
 
     // Submit read request for src file
     struct iocb read_req;
@@ -48,7 +48,7 @@ int do_copy(char *src_fp, char *dest_fp)
     io_prep_pread(&read_req, src_fd, buffer, fsize, 0);
     if (io_submit(aio_context, 1, &read_iocbs) != 1)
     {
-        printf("Failed to submit src file read request...EXITING\n");
+        perror("Failed to submit src file read request...");
         return -1;
     }
 
@@ -57,16 +57,16 @@ int do_copy(char *src_fp, char *dest_fp)
     while (io_getevents(aio_context, 0, 1, &read_events[0], NULL) == 0) {} //spin
     if (read_events[0].res < 0)
     {
-        printf("Failed to read src file...EXITING\n");
+        perror("Failed to read src file...");
         return -1;
     }
     close(src_fd);
 
     // Submit write request to dest file
-    int dest_fd = open(dest_fp, O_RDWR | O_CREAT | O_DIRECT, "a");
+    int dest_fd = open(dest_fp, O_RDWR | O_CREAT | O_DIRECT, 0666);
     if (dest_fd < 0)
     {
-        printf("Failed to open dest file...EXITING\n");
+        perror("Failed to open dest file...");
         return -1;
     }
     struct iocb write_req;
@@ -74,7 +74,7 @@ int do_copy(char *src_fp, char *dest_fp)
     io_prep_pwrite(&write_req, dest_fd, buffer, fsize, 0);
     if (io_submit(aio_context, 1, &write_iocbs) != 1)
     {
-        printf("Failed to submit dest file write request...EXITING\n");
+        perror("Failed to submit dest file write request...");
         return -1;
     }
 
@@ -83,11 +83,14 @@ int do_copy(char *src_fp, char *dest_fp)
     while (io_getevents(aio_context, 0, 1, &write_events[0], NULL) == 0) {} //spin
     if (write_events[0].res < 0)
     {
-        printf("Failed to write dest file...EXITING\n");
+        perror("Failed to write dest file...");
         return -1;
     }
-    io_destroy(aio_context);
-    free(buffer);
+    if (io_destroy(aio_context) != 0)
+    {
+        perror("Failed to destroy aio_context");
+        return -1;
+    }
     return 0;
 }
 
@@ -97,7 +100,7 @@ int main(int argc, char *argv[])
 
     // Get command line args
     if (argc < 3) {
-        printf("./copy_test src dest\n");
+        perror("./copy_test src dest\n");
         return -1;
     }
 
