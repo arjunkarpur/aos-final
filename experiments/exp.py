04bd354c786ec763603ad6fe89a4fd836486ae8d
Arjun Karpur <arjun.karpur@gmail.com>
2019-04-24 14:04:11 -0500

    Interleaving baseline and our copies to make results more reliable

--- experiments/exp.py
+++ experiments/exp.py
@@ -11,7 +11,7 @@ RUN_NAME = "test1"
 EXP_SKIP_LIST = []
 N_EXPERIMENTS = 5
 CLEAN_SRC = True
-VERBOSE = True
+VERBOSE = False
 
 PROJ_DIR = os.path.join("..")
 FS_DIR = os.path.join(PROJ_DIR, "experiments", "fs")
@@ -41,7 +41,7 @@ def time_command(cmd):
     return end-start
 
 def experiment_main(test_case):
-    log_print("[TEST CASE %i]" % test_case.case_id)
+    log_print("[TEST CASE ID: %i]" % test_case.case_id)
 
     # Create file system according to test case
     log_print("\tCreating src directory tree")
@@ -50,30 +50,30 @@ def experiment_main(test_case):
     os.makedirs(src_dir)
     test_case.gen_func(src_dir)
 
-    # Run tests for base cp -r
-    #TODO: baseline should be the same version of coreutils, but without any modifications. fix this
-    log_print("\tRunning test for baseline cp -r")
     base_times = []
-    for _ in range(N_EXPERIMENTS):
-        t = time_command("cp -r %s %s" % (src_dir, dest_dir))
-        base_times.append(t)
-        shutil.rmtree(dest_dir)
-        if VERBOSE:
-            log_print("\t      %i - %f" % (_+1, t))
-    log_print("\t    Avg - %f" % (sum(base_times)/float(N_EXPERIMENTS)))
-
-    # TODO: do we need to interleave tests? To account for differences in server state between the two sets of experiments
-
-    # Run tests for our cp -r
     our_times = []
-    log_print("\tRunning tests for our cp -r")
     for _ in range(N_EXPERIMENTS):
-        t = time_command("%s -r %s %s" % (os.path.join(PROJ_DIR, "coreutils-8.31", "src", "cp"), src_dir, dest_dir))
-        our_times.append(t)
+        # Cp commands for both
+        base_bin = "cp"
+        ours_bin = os.path.join(os.path.join(PROJ_DIR, "coreutils-8.31", "src", "cp"))
+        cp_args = "-r %s %s" % (src_dir, dest_dir)
+        base_cmd = "%s %s" % (base_bin, cp_args)
+        our_cmd = "%s %s" % (ours_bin, cp_args)
+
+        # Run cp commands
+        t_base = time_command(base_cmd)
         shutil.rmtree(dest_dir)
+        t_ours = time_command(our_cmd)
+        shutil.rmtree(dest_dir)
+
+        # Record results
+        base_times.append(t_base)
+        our_times.append(t_ours)
         if VERBOSE:
-            log_print("\t      %i - %f" % (_, t))
-    log_print("\t    Avg - %f" % (sum(our_times)/float(N_EXPERIMENTS)))
+            log_print("\t    Baseline %i/%i - %f" % (_+1, N_EXPERIMENTS, t_base))
+            log_print("\t        Ours %i/%i - %f" % (_+1, N_EXPERIMENTS, t_ours))
+    log_print("\t    Baseline Avg - %f" % (sum(base_times)/float(N_EXPERIMENTS)))
+    log_print("\t        Ours Avg - %f" % (sum(our_times)/float(N_EXPERIMENTS)))
 
     # Clean up generated directory tree
     log_print("\tCleaning up")
@@ -96,15 +96,19 @@ def log_results(results):
 def main():
     # Create all test cases
     test_cases = create_test_cases();
+    log_print("%i Test Cases" % len(test_cases))
 
     # Run experiment loop on each test case
+    log_print("Starting experiments...")
     results = []
     for tcase in test_cases:
         tc_results = experiment_main(tcase)
         results.append(tc_results)
 
     # Report results
+    log_print("Writing results to file...")
     log_results(results)
+    log_print("DONE!")
 
 if __name__ == "__main__":
     main()
--- experiments/test_case_gen.py
+++ experiments/test_case_gen.py
@@ -6,12 +6,12 @@ _GB_ = 1024*1024*1024
 
 def get_gen_func(i):
     if i == 1:
-        return one 
+        return _tc_one_
     else:
         pass
     return
 
-def one(root_dir):
+def _tc_one_(root_dir):
     # Single small file in directory
     with open(os.path.join(root_dir, "one.txt"), "wb") as f:
-        f.write(os.urandom(1*_KB_));
+        f.write(os.urandom(1*_MB_));
